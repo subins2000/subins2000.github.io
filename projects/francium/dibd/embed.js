91a726dd7bc556619b379569c053f397bf0b53c3
Subin Siby <subins2000@gmail.com>
2017-07-29 20:19:55 +0530

    Updated DIBD

--- projects/francium/dibd/embed.js
+++ projects/francium/dibd/embed.js
@@ -2,5 +2,5 @@
  * Disqus in Blogger Dynamic views integrated by Subin Siby [http://goo.gl/jPb8TR]
  */
 window.disqus_shortname = 'example'; // required: replace example with your forum shortname
-var d = document, o = d.createElement(&quot;script&quot;), o.type = &quot;text/javascript&quot;, o.async = true, o.src = &quot;http://lab.subinsb.com/projects/Francium/dibd/load.min.js&quot;;
+var d = document, o = d.createElement(&quot;script&quot;), o.type = &quot;text/javascript&quot;, o.async = true, o.src = &quot;https://lab.subinsb.com/projects/Francium/dibd/load.min.js&quot;;
 d.getElementsByTagName(&#39;head&#39;)[0].appendChild(o);
--- projects/francium/dibd/load.js
+++ projects/francium/dibd/load.js
@@ -1,45 +1,49 @@
 /**
- * Francium Disqus In Blogger Dynamic Views (DIBD) Plugin by Subin Siby [http://goo.gl/jPb8TR]
- * 2016-02-05
+ * Francium Disqus In Blogger Dynamic Views (DIBD) Plugin by Subin Siby [https://goo.gl/jPb8TR]
+ * 2017-07-29
  */
-(function(window){
-  window.Fr = window.Fr || {};
-  Fr.url = "", Fr.identifier = "";
-  window.disqus_url = Fr.url;
-  
-  if(location.hash.match("#comment") !== null){
-    Fr.commentHash = location.hash;
-  }
-  
-  Fr.loadDisqus = function(){
-    if(typeof Fr.commentHash != "undefined" && $('#disqus_thread').height() < 52){
-      window.location.hash = Fr.commentHash;
+(function(window) {
+    window.Fr = window.Fr || {};
+    Fr.url = "", Fr.identifier = "";
+    window.disqus_url = Fr.url;
+
+    if (location.hash.match("#comment") !== null) {
+        Fr.commentHash = location.hash;
     }
-    if($("#disqus_thread.subinsblogdisqus").length == 0 && $('.article-footer .share-controls').length != 0 && Fr.url != location.href){
-      console.log("DIBD (Disqus In Blogger Dynamic views) : http://goo.gl/jPb8TR http://subinsb.com/how-to-integrate-disqus-into-blogger-dynamic-views");
-      $('.article-footer .share-controls').after('<'+'div id="disqus_thread" class="subinsblogdisqus"><'+'/div><'+'a href="http://goo.gl/jPb8TR">Disqus for Blogger Dynamic views<'+'/a>');
-      if(typeof Fr.commentHash == "undefined"){
-        Fr.url = $(".article .title.entry-title:first a[href]").attr("href");
-        Fr.url = Fr.url == "" || typeof Fr.url == "undefined" ? window.location.href : Fr.url;
-      }
-      
-      a = document.createElement('a'), a.href = Fr.url, Fr.identifier = a.pathname;
-      
-      window.disqus_url = Fr.url, window.disqus_identifier = Fr.identifier;
-      DISQUS.reset({
-        reload: true,
-        config: function () {  
-          this.page.identifier = Fr.identifier;
-          this.page.url = Fr.url;
+
+    Fr.loadDisqus = function() {
+        if (typeof Fr.commentHash != "undefined" && $('#disqus_thread').height() < 52) {
+            window.location.hash = Fr.commentHash;
         }
-      });
+        if ($("#disqus_thread.subinsblogdisqus").length == 0 && $('.article-footer .share-controls').length != 0 && Fr.url != location.href) {
+            console.log("DIBD (Disqus In Blogger Dynamic views) : https://goo.gl/jPb8TR https://subinsb.com/how-to-integrate-disqus-into-blogger-dynamic-views");
+            $('.article-footer .share-controls').after('<' + 'div id="disqus_thread" class="subinsblogdisqus"><' + '/div><' + 'a href="https://goo.gl/jPb8TR">Disqus for Blogger Dynamic views<' + '/a>');
+            if (typeof Fr.commentHash == "undefined") {
+                Fr.url = $(".article .title.entry-title:first a[href]").attr("href");
+                Fr.url = Fr.url == "" || typeof Fr.url == "undefined" ? window.location.href : Fr.url;
+            }
+
+            a = document.createElement('a'), a.href = Fr.url, Fr.identifier = a.pathname;
+
+            window.disqus_url = Fr.url, window.disqus_identifier = Fr.identifier;
+            DISQUS.reset({
+                reload: true,
+                config: function() {
+                    this.page.identifier = Fr.identifier;
+                    this.page.url = Fr.url;
+                }
+            });
+        }
     }
-  }
-  
-  $(document).ready(function(){
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    setInterval(function(){
-      Fr.loadDisqus();
-    }, 5000);
-  });
+
+    $(document).ready(function() {
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        setInterval(function() {
+            Fr.loadDisqus();
+        }, 5000);
+    });
 })(window);
